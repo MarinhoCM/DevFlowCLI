8cb487438c1b0f283f81bdd8e2f9d9792da0ed3f
MarinhoCM <marinhoc384@gmail.com>
2026-02-15 11:39:10 -0300

    :sparkles: feat: inserido resgistro de horário para inserção de atividades

--- src/infra/database/sqlite/config/DatabaseConfig.java
+++ src/infra/database/sqlite/config/DatabaseConfig.java
@@ -24,8 +24,8 @@ public class DatabaseConfig {
                 title TEXT NOT NULL,
                 description TEXT,
                 done INTEGER NOT NULL DEFAULT 0,
-                created_at TEXT NOT NULL,
-                updated_at TEXT
+                created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+                updated_at TEXT DEFAULT CURRENT_TIMESTAMP
             )
         """;
 
